9942212fd71fd8f9144a41d282a8b86eae0f97bd
saralein <sara.m.trujillo@gmail.com>
2017-11-15 11:16:21 -0600

    Adds args flags for cob_spec.

--- src/main/java/com/saralein/server/validation/ArgsValidation.java
+++ src/main/java/com/saralein/server/validation/ArgsValidation.java
@@ -1,10 +1,14 @@
 package com.saralein.server.validation;
 
+import static com.saralein.server.Constants.*;
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArgsValidation {
     private String userLocation;
     private String port;
+    private String rootPath;
     private File root;
 
     public ArgsValidation(String userLocation) {
@@ -20,25 +24,57 @@ public class ArgsValidation {
     }
 
     public boolean argsAreValid(String[] args) {
-        setUpArgs(args);
+        List<String> argsList = Arrays.asList(args);
+
+        if (hasValidArgsFormat(argsList)) {
+            setupArgs(argsList);
+        } else {
+            return false;
+        }
 
         return validPort() && validDirectory();
     }
 
-    private void setUpArgs(String[] args) {
-        if(args.length == 0) {
-            setupPortAndRoot("1337", "Public");
+    private boolean hasValidArgsFormat(List<String> argsList) {
+        return argsHaveNoLength(argsList) || hasBothFlagsAndValidLength(argsList) || hasOneFlagAndValidLength(argsList);
+    }
+
+    private boolean argsHaveNoLength(List<String> argsList) {
+        return argsList.size() == 0;
+    }
+
+    private boolean hasBothFlagsAndValidLength(List<String> argsList) {
+        return hasFlag(argsList, PORT_FLAG) && hasFlag(argsList, DIRECTORY_FLAG) && argsList.size() == EXPECTED_ARGS_LENGTH;
+    }
+
+    private boolean hasOneFlagAndValidLength(List<String> argsList) {
+        return (hasFlag(argsList, PORT_FLAG) || hasFlag(argsList, DIRECTORY_FLAG)) &&
+                argsList.size() == EXPECTED_ARGS_LENGTH / 2;
+    }
+
+    private boolean hasFlag(List<String> argsList, String flag) {
+        return argsList.indexOf(flag) == 0 || argsList.indexOf(flag) == 2;
+    }
+
+    private void setupArgs(List<String> argsList) {
+        port = setupArg(argsList, PORT_FLAG);
+        rootPath = setupArg(argsList, DIRECTORY_FLAG);
+    }
+
+    private String setupArg(List<String> argsList, String flag) {
+        int flagIndex = argsList.indexOf(flag);
+        int argIndex = flagIndex + 1;
+
+        if(flagIndex != -1 && argIndex < argsList.size()) {
+            return argsList.get(argIndex);
         } else {
-            setupPortAndRoot(args[0], args[1]);
+            return flag == PORT_FLAG ? "1337" : "public";
         }
     }
 
-    private void setupPortAndRoot(String argPort, String argRoot) {
-        port = argPort;
-        root = new File(userLocation + File.separator + argRoot);
-    }
-
     private boolean validDirectory() {
+        root = new File(userLocation + File.separator + rootPath);
+
         return root.exists();
     }
 
--- src/test/java/com/saralein/server/validation/ArgsValidationTest.java
+++ src/test/java/com/saralein/server/validation/ArgsValidationTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 public class ArgsValidationTest {
     private ArgsValidation argsValidation;
+    private String[] emptyArgs;
     private String[] validArgs;
     private File root;
 
@@ -16,30 +17,69 @@ public class ArgsValidationTest {
         String rootPath = userLocation + File.separator + "public";
         root = new File(rootPath);
 
-        validArgs = new String[]{"6066", "public"};
+        emptyArgs = new String[]{};
+        validArgs = new String[]{"-p", "6066", "-d", "public"};
         argsValidation = new ArgsValidation(userLocation);
     }
 
     @Test
-    public void determinesIfArgumentsAreValid() {
-        String[] invalidArgs = new String[]{"port6066", "totally-made-up"};
-        String[] belowRange = new String[]{"-8000", "public"};
-        String[] aboveRange = new String[]{"70000", "public"};
+    public void returnsFalseWhenArgsHaveFlagsWithMissingSpecification() {
+        String[] justDir = new String[]{"-d"};
+        String[] justPort = new String[]{"-p"};
+        String[] missingPortFirst = new String[]{"-d", "sloths", "-p"};
+        String[] missingPortLast = new String[]{"-p", "-d", "sloths"};
+        String[] missingDirectoryFirst = new String[]{"-d", "-p", "6066"};
+        String[] missingDirectoryLast = new String[]{"-p", "6066", "-d"};
+
+        assertFalse(argsValidation.argsAreValid(justDir));
+        assertFalse(argsValidation.argsAreValid(justPort));
+        assertFalse(argsValidation.argsAreValid(missingPortFirst));
+        assertFalse(argsValidation.argsAreValid(missingPortLast));
+        assertFalse(argsValidation.argsAreValid(missingDirectoryFirst));
+        assertFalse(argsValidation.argsAreValid(missingDirectoryLast));
+    }
+
+    @Test
+    public void determinesIfArgsWithValidFormatHaveValidSpecifications() {
+        String[] onlyValidPort = new String[]{"-p", "6060"};
+        String[] onlyValidDir = new String[]{"-d", "public"};
+
+        String[] invalidSpecs = new String[]{"-p", "port6066", "-d", "totally-made-up"};
+        String[] flagsFirst = new String[]{"-p", "-d", "6060", "public"};
+        String[] flagsLast = new String[]{ "6060", "public", "-p", "-d"};
+        String[] belowRangePort = new String[]{"-p", "-8000", "-d", "public"};
+        String[] aboveRangePort = new String[]{"-p", "70000", "-d", "public"};
+        String[] onlyInvalidPort = new String[]{"-p", "70000"};
+        String[] onlyInvalidDir = new String[]{"-d", "-p"};
 
         assertTrue(argsValidation.argsAreValid(validArgs));
-        assertFalse(argsValidation.argsAreValid(invalidArgs));
-        assertFalse(argsValidation.argsAreValid(belowRange));
-        assertFalse(argsValidation.argsAreValid(aboveRange));
+        assertTrue(argsValidation.argsAreValid(emptyArgs));
+        assertTrue(argsValidation.argsAreValid(onlyValidPort));
+        assertTrue(argsValidation.argsAreValid(onlyValidDir));
+
+        assertFalse(argsValidation.argsAreValid(invalidSpecs));
+        assertFalse(argsValidation.argsAreValid(flagsFirst));
+        assertFalse(argsValidation.argsAreValid(flagsLast));
+        assertFalse(argsValidation.argsAreValid(belowRangePort));
+        assertFalse(argsValidation.argsAreValid(aboveRangePort));
+        assertFalse(argsValidation.argsAreValid(onlyInvalidPort));
+        assertFalse(argsValidation.argsAreValid(onlyInvalidDir));
     }
 
     @Test
     public void returnsDirectoryFromArgs() {
+        argsValidation.argsAreValid(emptyArgs);
+        assertEquals(root, argsValidation.determineRoot());
+
         argsValidation.argsAreValid(validArgs);
         assertEquals(root, argsValidation.determineRoot());
     }
 
     @Test
     public void returnsPortFromArgs() {
+        argsValidation.argsAreValid(emptyArgs);
+        assertEquals(new Integer(1337), argsValidation.parsePort());
+
         argsValidation.argsAreValid(validArgs);
         assertEquals(new Integer(6066), argsValidation.parsePort());
     }
